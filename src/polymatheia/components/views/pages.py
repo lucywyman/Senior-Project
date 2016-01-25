de82cf793b5b07834b562a8bc765068e1d433e34
Lucy Wyman <wyman.lucy@gmail.com>
2016-01-25 15:05:29 -0800

    Start to implement API, what even is a UI....

--- src/polymatheia/components/views/pages.py
+++ src/polymatheia/components/views/pages.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 def index(request):
     """
+    */
     Returns the index page for the site
     """
     return render(request, 'index.html')
 
 def about(request):
+    """
+    */about.html
+    Returns the about page for this project
+    """
     return render(request, 'about.html')
