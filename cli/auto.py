a73664d77fa09f00302a3142f9d32624825bf1d6
Nathan Hennig <hennign@oregonstate.edu>
2016-02-05 04:50:29 +0000

    Partially implemented submission command.

--- cli/auto.py
+++ cli/auto.py
@@ -8,7 +8,35 @@
 #   http://stackoverflow.com/questions/9973990/python-cmd-dynamic-docstrings-for-do-help-function
 #   http://stackoverflow.com/questions/16826172/filename-tab-completion-in-cmd-cmd-of-python
 
-import cmd, glob, os, sys
+import cmd, glob, os, requests, sys
+
+
+# for testing
+import logging
+
+
+# Enabling debugging at http.client level (requests->urllib3->http.client)
+# you will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# the only thing missing will be the response.body which is not logged.
+try: # for Python 3
+    from http.client import HTTPConnection
+except ImportError:
+    from httplib import HTTPConnection
+HTTPConnection.debuglevel = 1
+
+logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+
+
+
+
+
+
+
 
 def _append_slash_if_dir(p):
     if p and os.path.isdir(p) and p[-1] != os.sep:
@@ -41,6 +69,7 @@ class AutoShell(cmd.Cmd):
     # TODO Call server to identify user as student, ta,
     # or teacher.
     user = "student"
+    server = "http://127.0.0.1:8000/"
 
     intro = 'Welcome to the AUTO Universal Testing Organizer (AUTO) shell.\n   Type help or ? to list commands'
     prompt = '>> '
@@ -81,7 +110,23 @@ class AutoShell(cmd.Cmd):
 
     def do_submission(self, args):
         'Add, View, Update Submissions'
-        print("Not implemented")
+        args = args.split()
+        if args[0]=="add":
+            try:
+                int(args[1])
+            except ValueError:
+                print("Error: assignment-id must be an integer value.")
+                return
+            url = self.server + 'submission/'
+            data = { "assignment-id": args[1] }
+            files = { "file": open(args[2]) }
+            r = requests.post(url, files=files, data=data)
+            
+            # TODO more robust error reporting
+            if r.status_code==200:
+                print("Submission succeeded!")
+            else:
+                print("Failed")
 
     def complete_submission(self, text, line, begidx, endidx):
         before_arg = line.rfind(" ", 0, begidx)
@@ -105,11 +150,6 @@ class AutoShell(cmd.Cmd):
     def do_ce(self, args):
         'Add, View, Update, Delete Common Errors'
         print("Not implemented")
-
-    def do_submission(self, args):
-        'Add, View, Update Submissions'
-        print("Not implemented")
-
 
 
 def parse(arg):
