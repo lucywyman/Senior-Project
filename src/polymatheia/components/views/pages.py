ce439c78a62433e620a4b8e6e73327e8a6dfc9db
Lucy Wyman <wyman.lucy@gmail.com>
2016-03-11 16:51:32 -0500

    Start implementing api

--- src/polymatheia/components/views/pages.py
+++ src/polymatheia/components/views/pages.py
@@ -1,13 +1,18 @@
 from django.http import *
 from django.shortcuts import *
+from django.conf import settings
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.core.context_processors import csrf
 from polymatheia.components.forms import *
+import datetime, json, requests
 
 def index(request):
-	return render_to_response('index.html')
+    data = {'required':''}
+    user = requests.post(settings.API_IP+'student/view', json=data)
+    course = requests.post(settings.API_IP+'course/view', json=data)
+    return render_to_response('index.html', {'n':datetime.datetime.now(), 'user':user, 'course':course})
 
 def about(request):
 	return render_to_response('about.html')
