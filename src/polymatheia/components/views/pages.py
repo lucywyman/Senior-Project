470b177bd20cf9a07f113cf857dae3444ea5d676
Lucy Wyman <wyman.lucy@gmail.com>
2016-03-11 15:29:53 -0800

    Index page works! API communication working

--- src/polymatheia/components/views/pages.py
+++ src/polymatheia/components/views/pages.py
@@ -9,10 +9,21 @@ from polymatheia.components.forms import *
 import datetime, json, requests
 
 def index(request):
-    data = {'required':''}
-    user = requests.post(settings.API_IP+'student/view', json=data)
-    course = requests.post(settings.API_IP+'course/view', json=data)
-    return render_to_response('index.html', {'n':datetime.datetime.now(), 'user':user, 'course':course})
+    api_ip = settings.API_IP
+    user_data = {'student':['hennign']}
+    userobj = requests.get(api_ip+'student/view', json=user_data)
+    user = userobj.json()
+    for i in range(len(user)):
+        user[i]['type'] = 'student'
+    courses = []
+    for course in user:
+        course_data = {'course-id':[course['course_id']]}
+        cobj = requests.get(api_ip+'course/view', json=course_data)
+        c = cobj.json()
+        aobj = requests.get(api_ip+'assignment/view')
+        c[0]['assignments'] = aobj.json()
+        courses.append(c[0])
+    return render_to_response('index.html', {'n':datetime.datetime.now(), 'user':user[0], 'courses':courses})
 
 def about(request):
 	return render_to_response('about.html')
