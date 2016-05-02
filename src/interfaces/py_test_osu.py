ff8294bff2b81e527db731002065ddb43e3773d8
hennign <hennign@onid.oregonstate.edu>
2016-05-02 06:16:03 -0400

    Fixed bugs in Python interface and sample test.

--- src/interfaces/py_test_osu.py
+++ src/interfaces/py_test_osu.py
@@ -1,48 +1,55 @@
 import operator
 import json
 import sys
+import psycopg2
+import psycopg2.extras
 
 class test_suite:
     def __init__(self):
+        # note: when this line was farther down, it was erasing
+        # record of attributes above it
+        self.__dict__ = {}
+
         self.testCount = 0
         self.testsRemain = 0
         self.TAPstring = ''
         self.weightTotal = 0
         self.weightAcc = 0
         self.jsonRet = json.loads('{"TAP":"","Tests":[],"Errors":[],"Grade":""}')
-        self.__dict__ = {}
         self._connectVars = ''
         self.testID = 0
-        self._connectVars = sys.argv[1]
-        self.sub_ID = sys.argv[2]
-        self.test_ID = sys.argv[3]
+        self._connectVars = "{0} {1} {2}".format(sys.argv[1], sys.argv[2], sys.argv[3])
+        self.sub_ID = sys.argv[4]
+        self.test_ID = sys.argv[5]
         sys.argv[1] = "You shouldn't be looking at this."
         sys.argv[2] = 0
         sys.argv[3] = 0
-        
+        sys.argv[4] = 0
+        sys.argv[5] = 0
+
     #Helper functions
     def ok(self,message,weight):
         self.testCount += 1
         self.testsRemain -= 1
-        if(self.testsRemain < 0): 
+        if(self.testsRemain < 0):
             print('# Exceeded declared test count for this "describe"!')
             self.jsonRet['Errors'].append("Exceeded declared test count for a describe.")
-        logj('ok ',self.testCount,message,weight)
+        self.logj('ok ',self.testCount,message,weight)
     def notok(self,message,weight):
         self.testCount += 1
         self.testsRemain -= 1
-        if(self.testsRemain < 0): 
+        if(self.testsRemain < 0):
             print('# Exceeded declared test count for this "describe"!')
             self.jsonRet['Errors'].append("Exceeded declared test count for a describe.")
-        logj('not ok',self.testCount,message,weight)
-    
+        self.logj('not ok',self.testCount,message,weight)
+
     def logj(self,ok,testNumber,message,weight):
-      testj = json.loads('{"state":'+ok+',"testNumber":'+str(testNumber)+',"message":'+message+',"weight":'+str(weight)+'}')
+      testj = json.loads('{"state":"'+ok+'","testNumber":'+str(testNumber)+',"message":"'+message+'","weight":'+str(weight)+'}')
       self.jsonRet['Tests'].append(testj)
       self.TAPstring += ' '.join([ok,str(testNumber),message,str(weight)]) + '\n'
-    
-    def conclude(self)
-      if(self.testsRemain > 0): 
+
+    def conclude(self):
+      if(self.testsRemain > 0):
           print('# More tests declared than executed!')
           self.jsonRet['Errors'].append("More tests declared than executed.")
       self.jsonRet['TAP'] = self.TAPstring
@@ -51,30 +58,30 @@ class test_suite:
       conn.autocommit = True
       cur = conn.cursor()
       cur.execute("""
-                        INSERT INTO submissions_have_results (submission_id, test_id, results)
-                        VALUES (%s, %s, %s)
-                        """, (self.sub_ID, self.test_ID, json.dumps(self.jsonRet))
-                        )
+        INSERT INTO submissions_have_results (submission_id, test_id, results)
+        VALUES (%s, %s, %s)
+        """, (self.sub_ID, self.test_ID, json.dumps(self.jsonRet))
+        )
       cur.close()
-      
+
     #Testing functions
-    def assert_equals(self,actual,expected,message,weight=0):
+    def assert_equals(self,actual,expected,message,weight=1):
         self.weightTotal += weight
         if(operator.eq(actual,expected)):
             self.ok(message,weight)
             self.weightAcc += weight
         else:
             self.notok(message,weight)
-            
-    def assert_not_equals(self,actual,unexpected,message,weight=0):
+
+    def assert_not_equals(self,actual,unexpected,message,weight=1):
         self.weightTotal += weight
         if(operator.ne(actual,unexpected)):
             self.ok(message,weight)
             self.weightAcc += weight
         else:
             self.notok(message,weight)
-            
-    def expect_error(self,message,thunk,weight=0):
+
+    def expect_error(self,message,thunk,weight=1):
         self.weightTotal += weight
         try:
             thunk()
@@ -82,19 +89,19 @@ class test_suite:
         except:
             self.ok(message,weight)
             self.weightAcc += weight
-            
-    def expect(self,passed,message,weight=0):
+
+    def expect(self,passed,message,weight=1):
         self.weightTotal += weight
         if(passed):
             self.ok(message,weight)
             self.weightAcc += weight
         else:
             self.notok(message,weight)
-            
+
     #Grouping functions
     def describe(self,message,tests):
-        self.TAPstring += str(self.testCount + 1) + '..' + str(self.testCount + tests))
-        self.TAPstring += '# ' + message
+        self.TAPstring += str(self.testCount + 1) + '..' + str(self.testCount + tests)
+        self.TAPstring += '# ' + message + '\n'
         self.testsRemain = tests
     def it(self,message):
         self.TAPstring += '# ' + message
--- src/test/test.py
+++ src/test/test.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3.2
+#!/usr/bin/env python3
 from py_test_osu import test_suite
 import dummy
 
@@ -9,3 +9,4 @@ test.assert_equals(1,1,'one equals one')
 test.assert_not_equals(1,2,'one does not equal two')
 test.expect(1==1,'a true statement')
 test.assert_equals('hello',dummy.dummyfunc(),'testing works?')
+test.conclude()
