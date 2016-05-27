dac6222c79505cff8bc1672655f327b599aa7693
hennign <hennign@onid.oregonstate.edu>
2016-05-27 02:26:56 -0400

    Wrote simple test, Make, and submission files for C interface.

---
+++ src/test/c/student.c
@@ -0,0 +1,5 @@
+#include "student.h"
+
+char * dummyfunc(void) {
+    return "hello";
+}
---
+++ src/test/c/student.h
@@ -0,0 +1,8 @@
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
+
+char * dummyfunc(void);
+
+
+#endif //STUDENT_H_
